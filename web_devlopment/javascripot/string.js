ec0c6ba09c7ead65ab279d3aaf93bd01d3a09408
ankitkr0209 <ankit.kumar02sep@gmail.com>
2024-03-06 23:04:35 +0530

    string methods

---
+++ web_devlopment/javascripot/string.js
@@ -0,0 +1,23 @@
+const Name = "ankit";
+const  course = "BCA"
+console.log(`Hello my name is ${Name} . I am doing ${course} course. `)
+
+console.log(typeof Name)
+
+
+const gamename = new String("    Asphalt Nitro   ")
+console.log(typeof gamename);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.trim());
+console.log(gamename.charAt(9));
+console.log(gamename.indexOf('l'));
+console.log(gamename.substring(4,10))
+console.log(gamename.slice(-11,18))
+const intro = "my name is ankit kumar"
+
+// split covert the string into array as mentioned below
+
+console.log(intro.split(" "))
