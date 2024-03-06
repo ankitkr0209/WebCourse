aecdc93c5f65dd61a503b5a8cd2aec5435b91418
ankitkr0209 <ankit.kumar02sep@gmail.com>
2024-03-06 23:46:42 +0530

    methods of numbers and maths library

---
+++ web_devlopment/javascripot/nums_maths.js
@@ -0,0 +1,36 @@
+const num = 400
+// console.log(typeof num)
+// console.log(num)
+
+const num1 = new Number(10000980.98755)
+// console.log(num1)
+
+// covertin number to string 
+// console.log(num1.toString().length)
+
+// console.log(num1.toFixed(2))
+
+// console.log(num1.toPrecision(7))
+// console.log(num1.toLocaleString("em-IN"))
+
+// *****************MATHS***************************
+
+
+// console.log( typeof Math)
+// console.log(Math.abs(-1234))
+// console.log(Math.round(234.8342))
+// console.log(Math.ceil(32.323))
+// console.log(Math.floor(32.923))
+// console.log(Math.min(2,3,4,5,6,3,4))
+// console.log(Math.max(4,2,7,89,4,54,4))
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor((Math.random()*10) +1))
+
+// printing the numer between the given range 
+// example :- below we are printing the the random value between 40 - 50 
+
+console.log(Math.floor((Math.random() * (50-40 +1 )) +40))
+
